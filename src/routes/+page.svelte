93039d39dda8a31f0d13e9107eaef3960dd8c242
Jojo <aditya.abdul@weekendinc.com>
2024-04-18 01:00:15 +0700

    use right fonts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+	<div class="topText">#FINDYOUR</div>
+</main>
 
 <style>
+	main {
+		position: relative;
+		min-height: 100vh;
+		background-color: orangered;
+		color: #fff;
+	}
+
+	.topText {
+		text-align: center;
+		font-size: 40px;
+		letter-spacing: 24px;
+		font-weight: 500;
+	}
+
 	@font-face {
 		font-family: 'dahlia';
 		font-style: normal;
 		src: url('/fonts/dahlia/dahlia-medium.otf'); /* IE9 Compat Modes */
 	}
 
-	@font-face {
-		font-family: 'ag book pro';
-		font-style: normal;
-		font-weight: 400;
-		src: url('/fonts/ag-book-pro/AGBookRounded-Regular.otf');
-	}
-
 	* {
-		font-family: 'ag book pro';
+		font-family: 'Rubik', sans-serif;
+		font-optical-sizing: auto;
+		font-style: normal;
+		box-sizing: border-box;
 	}
 </style>
