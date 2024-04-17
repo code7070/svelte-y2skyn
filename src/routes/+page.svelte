758069ca23e1d6ac395755c6e7ac9a281a08c4be
Jojo <aditya.abdul@weekendinc.com>
2024-04-17 22:53:48 +0700

    initial

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+	@font-face {
+		font-family: 'dahlia';
+		font-style: normal;
+		src: url('/fonts/dahlia/dahlia-medium.otf'); /* IE9 Compat Modes */
+	}
+
+	@font-face {
+		font-family: 'ag book pro';
+		font-style: normal;
+		font-weight: 400;
+		src: url('/fonts/ag-book-pro/AGBookRounded-Regular.otf');
+	}
+
+	* {
+		font-family: 'ag book pro';
+	}
+</style>
