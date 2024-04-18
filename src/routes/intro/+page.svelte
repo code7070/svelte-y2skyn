a655ea690235d17f75b3113e4cf9e3483f2808e7
Jojo <aditya.abdul@weekendinc.com>
2024-04-18 14:15:53 +0700

    change assets methods

--- src/routes/intro/+page.svelte
+++ src/routes/intro/+page.svelte
@@ -1,5 +1,12 @@
 <script>
-	const domes = ['dome-1', 'dome-2', 'dome-3', 'dome-4', 'dome-5', 'dome-6', 'dome-7'];
+	import dome1 from '$lib/assets/intros/dome-1-low.webp';
+	import dome2 from '$lib/assets/intros/dome-2-low.webp';
+	import dome3 from '$lib/assets/intros/dome-3-low.webp';
+	import dome4 from '$lib/assets/intros/dome-4-low.webp';
+	import dome5 from '$lib/assets/intros/dome-5-low.webp';
+	import dome6 from '$lib/assets/intros/dome-6-low.webp';
+
+	const domes = ['dome-6', 'dome-2', 'dome-3', 'dome-4', 'dome-5', 'dome-6', 'dome-7'];
 	const steps = ['circle', ...domes];
 
 	let index = 0;
@@ -21,14 +28,12 @@
 
 <main>
 	<div class="circle" />
-	{#each domes as dome}
-		<img
-			class="imgDome"
-			aria-current={view === dome}
-			alt={dome}
-			src={`/images/intros/${dome}.webp`}
-		/>
-	{/each}
+	<img class="imgDome" aria-current={view === 'dome-1'} alt={'dome-1'} src={dome1} />
+	<img class="imgDome" aria-current={view === 'dome-2'} alt={'dome-2'} src={dome2} />
+	<img class="imgDome" aria-current={view === 'dome-3'} alt={'dome-3'} src={dome3} />
+	<img class="imgDome" aria-current={view === 'dome-4'} alt={'dome-4'} src={dome4} />
+	<img class="imgDome" aria-current={view === 'dome-5'} alt={'dome-5'} src={dome5} />
+	<img class="imgDome" aria-current={view === 'dome-6'} alt={'dome-6'} src={dome6} />
 </main>
 
 <style>
